0c7b0b85c1a0c133746873f68dd8b9846445832f
Jongseo Lee <jongseo12345@gmail.com>
2024-07-24 16:08:32 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,9 +6,7 @@ module.exports = function(app) {
     createProxyMiddleware({
       target: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000',
       changeOrigin: true,
-      pathRewrite: {
-        '^/api': '',
-      },
+      
     })
   );
 };
